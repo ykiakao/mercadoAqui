cd7f49673235dff3bab36679a1a935bc53c03f62
ykiakao <ykiakao@gmail.com>
2025-05-12 20:59:13 -0500

    Telas e funcionalidades

--- app/auth/login.js
+++ app/auth/login.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity, Alert, Image } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
@@ -16,8 +16,8 @@ export default function Login() {
     if (email === emailCorreto && senha === senhaCorreta) {
       setIsLoggedIn(true);
       setTimeout(() => {
-        router.replace('/protected'); // ✅ redireciona para a Home/tabbar
-      }, 100); // pequeno delay para garantir que o contexto atualize
+        router.replace('/protected');
+      }, 100);
     } else {
       Alert.alert('Erro', 'Email ou senha incorretos.');
     }
@@ -28,15 +28,27 @@ export default function Login() {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>Login</Text>
+    <View style={{ flex: 1, padding: 20, paddingTop: 60, backgroundColor: 'white', justifyContent: 'flex-start' }}>
+      <Image
+        source={require('../../assets/images/logo.png')}
+        style={{
+          width: 240,
+          height: 240,
+          resizeMode: 'contain',
+          alignSelf: 'center',
+          marginBottom: 10
+        }}
+      />
+
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' }}>Login</Text>
 
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
-        style={{ borderWidth: 1, marginBottom: 10, padding: 8 }}
+        keyboardType="email-address"
+        style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 8, marginBottom: 12, padding: 10 }}
       />
 
       <TextInput
@@ -44,13 +56,13 @@ export default function Login() {
         value={senha}
         onChangeText={setSenha}
         secureTextEntry
-        style={{ borderWidth: 1, marginBottom: 20, padding: 8 }}
+        style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 8, marginBottom: 20, padding: 10 }}
       />
 
-      <Button title="Entrar" onPress={handleLogin} />
+      <Button title="Entrar" color="#007BFF" onPress={handleLogin} />
 
       <TouchableOpacity onPress={irParaCadastro} style={{ marginTop: 20 }}>
-        <Text style={{ color: 'blue', textAlign: 'center' }}>
+        <Text style={{ color: '#007BFF', textAlign: 'center' }}>
           Ainda não tem conta? Cadastre-se
         </Text>
       </TouchableOpacity>
